189a98195546fdefb2129b684c66e44db6010f22
mathewbernard <mathew01088004@hotmail.com>
2026-08-27 15:33:46 -0400

    d

--- cours1.py
+++ cours1.py
@@ -1,1 +1,5 @@
+# auteur : Mathew Bernard
+# date : 2026-08-25
+# description : démonstration du cours 1
+
 print("allo les amis")
--- cours2.py
+++ cours2.py
@@ -1,1 +1,5 @@
-print("test")
+# auteur : Mathew Bernard
+# date : 2026-08-27
+# description : démonstration du cours 2
+
+print("test") # le print d'un test...
