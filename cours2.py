c2c7a6ac54c80560d2a8a2897e4e7ead66e39a2c
mathewbernard <mathew01088004@hotmail.com>
2026-08-27 16:55:59 -0400

    Update cours2.py

--- cours2.py
+++ cours2.py
@@ -2,10 +2,20 @@
 # date : 2026-08-27
 # description : démonstration du cours 2
 
-print("test") # le print d'un test...
+# print("test") # le print d'un test...
 
-a = 10
-b = 20
-c = a + b
-print(c)
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+
+nom = "Mathew"
+age = 17
+# print() peut accepter plusieurs arguments entre les ( )
+print("Bonjour", nom, ", tu as", age, "ans")
+
+# Chaîne formatée, ou « f-string »
+message = f"Bonjour {nom}, tu as {age} ans."
+print(message)
 
