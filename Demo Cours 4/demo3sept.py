9cd75ca9e698e9ca49d43fbe1940adacea07c01f
mathewbernard <mathew01088004@hotmail.com>
2026-09-03 16:18:53 -0400

    Update demo3sept.py

--- Demo Cours 4/demo3sept.py
+++ Demo Cours 4/demo3sept.py
@@ -1,10 +1,52 @@
-print("hello!")
+# print("hello!")
 
-nom_du_professeur = input("Quel est le nom de ton professeur? " )
-print(f"Le nom de mon professeur est {nom_du_professeur}")
+# nom_du_professeur = input("Quel est le nom de ton professeur? " )
+# print(f"Le nom de mon professeur est {nom_du_professeur}")
 
-age = int(input("Quel est ton âge? "))
-print(type(age))
+# age = int(input("Quel est son âge? "))
+# print(type(age))
 
 
-#age = age + 1
+# age = age + 1
+# print(f"L'age + 1 de ton prof est {age}")
+
+
+ami1 = "David"
+ami2 = "Benoit"
+ami3 = "Pierre"
+
+# print(ami1,ami2,ami3, sep=",")
+
+print("a", end="")
+print("b", end="")
+print("c", end="")
+print("")
+
+
+# import math
+
+# VALEUR_DE_PI = math.pi
+# print(f"Valeur de Pi : {VALEUR_DE_PI:.0f}")
+# print(f"Valeur de Pi : {VALEUR_DE_PI:.1f}")
+# print(f"Valeur de Pi : {VALEUR_DE_PI:.2f}")
+# print(f"Valeur de Pi : {VALEUR_DE_PI:.3f}")
+# print(f"Valeur de Pi : {VALEUR_DE_PI:.4f}")
+# print(f"Valeur de Pi : {VALEUR_DE_PI:.5f}")
+# print(f"Valeur de Pi : {VALEUR_DE_PI:.6f}")
+# print(f"Valeur de Pi : {VALEUR_DE_PI:.7f}")
+# print(f"Valeur de Pi : {VALEUR_DE_PI:.8f}")
+# print(f"Valeur de Pi : {VALEUR_DE_PI:.9f}")
+
+
+animal1 = "Chien"
+animal2 = "Chat"
+animal3 = "Renard"
+animal4 = "Raton laveur"
+
+print("----------------")
+print(f"|{animal1:>12}|") #padding
+print(f"|{animal2:>12}|")
+print(f"|{animal3:>12}|")
+print(f"|{animal4:>12}|")
+print("----------------")
+
