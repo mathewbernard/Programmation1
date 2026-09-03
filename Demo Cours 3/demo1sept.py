b1bdddd61cbfe14131b25e5e1adabc4c8416603d
mathewbernard <mathew01088004@hotmail.com>
2026-09-03 15:17:29 -0400

    c

---
+++ Demo Cours 3/demo1sept.py
@@ -0,0 +1,44 @@
+# VITESSE_DE_LA_LUMIERE = 300000
+# NB_SECONDES_PAR_MINUTE = 60
+
+
+# a = 10
+# b = 3
+
+# a = a + 10 # addition de 10 a la variable a
+# a = a - 10 # soustraction de 10 a la variable a
+# a = a * 10 # multiplication de 10 a la variable a
+
+# a = a / 10 # division de 10 a la variable a
+# a = a / b # division de 10 par 3
+
+
+
+
+
+# nb_tomates = 9
+# quantite_par_sac = 5
+
+# nb_de_sacs = nb_tomates // quantite_par_sac
+# print(f"Nombre de sac(s): {nb_de_sacs}")
+
+# nb_tomates_restantes = nb_tomates % quantite_par_sac
+# print(f"Nombre de tomates restantes: {nb_tomates_restantes}")
+
+
+# nb_secondes_totales = 97
+# nb_minutes = nb_secondes_totales // NB_SECONDES_PAR_MINUTE
+# nb_secondes_restantes = nb_secondes_totales % NB_SECONDES_PAR_MINUTE
+
+# print(f"{nb_minutes}m {nb_secondes_restantes}s")
+
+
+
+# a = 2 ** 3 # 2 exposant 3
+
+# a = (a * 2 + 2) - (3 / 4 ** 2)
+
+# print(a)
+
+a = 17 % 5
+print(a)
---
+++ Demo Cours 4/demo3sept.py
@@ -0,0 +1,10 @@
+print("hello!")
+
+nom_du_professeur = input("Quel est le nom de ton professeur? " )
+print(f"Le nom de mon professeur est {nom_du_professeur}")
+
+age = int(input("Quel est ton âge? "))
+print(type(age))
+
+
+#age = age + 1
