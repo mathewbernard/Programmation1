54dd4785d33b61349da6b9169c9e4071458458ca
mathewbernard <mathew01088004@hotmail.com>
2026-09-03 16:29:29 -0400

    Update demo3sept.py

--- Demo Cours 4/demo3sept.py
+++ Demo Cours 4/demo3sept.py
@@ -43,10 +43,15 @@ animal2 = "Chat"
 animal3 = "Renard"
 animal4 = "Raton laveur"
 
-print("----------------")
-print(f"|{animal1:>12}|") #padding
-print(f"|{animal2:>12}|")
-print(f"|{animal3:>12}|")
-print(f"|{animal4:>12}|")
-print("----------------")
+age_animal1 = 10
+age_animal2 = 11
+age_animal3 = 12
+age_animal4 = 13
+
+print("--------------------")
+print(f"|{animal1:>12}|{age_animal1:^5}|") #padding aligné à gauche
+print(f"|{animal2:<12}|{age_animal2:^5}|") #padding aligné à droite
+print(f"|{animal3:^12}|{age_animal3:^5}|") #padding aligné au centre
+print(f"|{animal4:^12}|{age_animal4:^5}|")
+print("--------------------")
 
