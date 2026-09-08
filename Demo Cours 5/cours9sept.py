72cfb76c6246c518fcbe76a6f0439b4234b1efbb
mathewbernard <mathew01088004@hotmail.com>
2026-09-08 10:49:31 -0400

    Add float equality demo using math.isclose
    Replace the simple age comparison with commented examples of comparison operators and add a floating-point equality demonstration. Introduces math import and shows that 0.1 + 0.2 != 0.3 using ==, then demonstrates a correct comparison using math.isclose. No behavioural logic beyond illustrative examples was changed.

--- Demo Cours 5/cours9sept.py
+++ Demo Cours 5/cours9sept.py
@@ -1,5 +1,38 @@
-age1 = 18
+age1 = 20
 age2 = 20
 
-print(age1 < age2)
 
+
+print(not (age1 != age2))
+if True:
+    print("c'est vrai")
+
+# plus_petit = age1 < age2
+# plus_grand = age1 > age2
+
+# plus_petit_ou_egal = (age1 <= age2)
+# plus_grand_ou_egal = (age1 >= age2)
+
+# different_de = (age1 != age2)
+
+
+
+# age_similaire = (age1 == age2)
+# print(age_similaire)
+
+
+# import math
+
+
+# variable_flottante = 0.1 + 0.2
+# variable_flottante2 = 0.3
+
+
+# ### pour une variable flottante... C'EST MAL
+# print(variable_flottante2 == variable_flottante)
+
+# ###pour une variable flottante... C'EST MIEUX
+# print(math.isclose(variable_flottante, variable_flottante2))
+
+
+# valeur = (variable_flottante2 == variable_flottante) and (a < b) and (a > c)
