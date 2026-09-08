d122901ce83d54a43aca1c6b532717437614ff30
mathewbernard <mathew01088004@hotmail.com>
2026-09-08 11:16:51 -0400

    Update cours9sept.py

--- Demo Cours 5/cours9sept.py
+++ Demo Cours 5/cours9sept.py
@@ -1,24 +1,24 @@
-age1 = 20
-age2 = 20
+# age1 = 20
+# age2 = 20
 
 
 
-print(not (age1 != age2))
-if True:
-    print("c'est vrai")
-
-# plus_petit = age1 < age2
-# plus_grand = age1 > age2
-
-# plus_petit_ou_egal = (age1 <= age2)
-# plus_grand_ou_egal = (age1 >= age2)
-
-# different_de = (age1 != age2)
+# print(not (age1 != age2))
 
 
 
-# age_similaire = (age1 == age2)
-# print(age_similaire)
+# # plus_petit = age1 < age2
+# # plus_grand = age1 > age2
+
+# # plus_petit_ou_egal = (age1 <= age2)
+# # plus_grand_ou_egal = (age1 >= age2)
+
+# # different_de = (age1 != age2)
+
+
+
+# # age_similaire = (age1 == age2)
+# # print(age_similaire)
 
 
 # import math
@@ -34,5 +34,12 @@ if True:
 # ###pour une variable flottante... C'EST MIEUX
 # print(math.isclose(variable_flottante, variable_flottante2))
 
+# # valeur = (variable_flottante2 == variable_flottante) and (a < b) and (a > c)
 
-# valeur = (variable_flottante2 == variable_flottante) and (a < b) and (a > c)
+
+avoir_faim = True
+age_du_client = 2
+genre_client = "fémini"
+
+client_possible = ((avoir_faim == True) or ((age_du_client >= 18) and (genre_client == "féminin")))
+print(f"Client possible? : {client_possible}")
