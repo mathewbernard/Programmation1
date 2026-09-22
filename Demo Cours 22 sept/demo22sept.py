f49633974ff88362880bfe21508d1fd3662ed0f7
mathewbernard <mathew01088004@hotmail.com>
2026-09-22 11:00:47 -0400

    Update demo22sept.py

---
+++ Demo Cours 22 sept/demo22sept.py
@@ -0,0 +1,74 @@
+# print("Début")
+
+
+# valeur = input("Veux-tu être mon ami? ").lower().strip()
+# if(valeur == "oui"):
+#     print("Merci!")
+# else:
+#     print("Sorry!")
+
+NOMBRE_CACHE = 5
+
+
+
+while True:
+    nombre = int(input("inscrire un nombre: "))
+    if(nombre == NOMBRE_CACHE):
+        print("Trouvé!")
+        break
+
+
+
+# compteur = 0
+
+
+
+# sortie = False
+# while not sortie:
+#     valeur_de_devinette = int(input("Inscrire un chiffre: "))
+
+#     if(valeur_de_devinette == NOMBRE_CACHE):
+#         print(f"Bravo! C'était bel et bien {NOMBRE_CACHE}")
+#         sortie = True
+    
+
+
+# i = 0
+# j = 0
+
+# taille = int(input("Quelle est la taille du carré: "))
+
+# while i < taille :
+#     j = 0
+#     while j < taille:
+#        print("*", end="")
+#        j += 1
+#     i += 1
+#     print("")
+
+
+
+
+ 
+
+# print("fin")
+
+
+
+
+# compteur = 10
+
+# while compteur > 0:
+#     print(compteur)
+#     compteur -= 1
+
+
+# print("Décollage!")
+
+
+
+
+
+
+
+
