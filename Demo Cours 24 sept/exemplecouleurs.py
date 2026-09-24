7781267a758b0e40a1e804cf112a558c7ca1bc9b
mathewbernard <mathew01088004@hotmail.com>
2026-09-24 15:47:00 -0400

    Create exemplecouleurs.py

---
+++ Demo Cours 24 sept/exemplecouleurs.py
@@ -0,0 +1,18 @@
+from colorama import Fore, Back, Style, init
+init(autoreset=True)
+
+# nom = input(f"Votre nom : {Fore.GREEN}")
+# # print(end=Style.RESET_ALL)  # on remet à zéro, sinon le print suivant est aussi coloré!
+# print(f"Bienvenue, {Fore.GREEN}{nom}")
+# print(f"{Back.GREEN}{Fore.BLUE} Le texte que je veux")
+
+
+print(f" Le verdict est : {Back.RED}URGENT")
+print(f" Le verdict est : {Back.GREEN}Normal")
+print(f" Le verdict est : {Back.YELLOW}À surveiller")
+
+
+
+
+print(f"{Style.RESET_ALL}")
+print("J'aime la pizza!")
